14d5ebb9dac07a4d3cb47b79b22a085076ea6aee
Jaep Cuperus <jaepcuperus@hotmail.com>
2019-01-10 13:24:08 +0100

    Fixed issue when loading css and javascript over http

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,9 +11,9 @@
     <title>Products list</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 </head>
     <body>
